c922d0f35c70771e70aab96c3aa5108d31af2e35
greduan <me@greduan.com>
2015-04-22 21:54:20 -0500

    bootstrap.sh makes symlinks to agno commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,10 @@
 mkdir bin destdir man meta packages source work
 
 git clone https://github.com/agnopm/ports ports
+
+cd bin
+ln -s ../agno_add ./agnoadd
+ln -s ../agno_info ./agnoinfo
+ln -s ../agno_ls ./agnols
+ln -s ../agno_mk ./agnomk
+ln -s ../agno_rm ./agnorm
