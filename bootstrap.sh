6330d6169181c8beba0922eaa521aae63cf8d12c
greduan <me@greduan.com>
2015-05-05 18:32:06 -0500

    agnoadd and agnomk got some niceties
    agnomk:
    Now checks if file exists before trying to download it, if it does exist
    then it skips the download and informs the user.
    
    agnoadd:
    Checks if provided package file exists, exits if it doesn't.
    
    It also checks if the file is installed (i.e. can be found in
    $AGNO_META), if it is installed then it first uninstalls it and then
    installs it again.
    
    bootstrap.sh:
    ln -s -> ln -sf.  I just figured out that the commands I had under
    usr/bin/ weren't symlinks, but rather copies of the original files (for
    whatever reason), so the last commits I wasn't actually testing.  I did
    this in order to make sure the symlinking actually does something.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@ mkdir -p meta packages source usr/{bin,include,lib,man,share}
 git clone --depth 1 https://github.com/agnopm/ports ports
 
 cd usr/bin
-ln -s ../../agno_add ./agnoadd
-ln -s ../../agno_ls ./agnols
-ln -s ../../agno_mk ./agnomk
-ln -s ../../agno_rm ./agnorm
+ln -sf ../../agno_add ./agnoadd
+ln -sf ../../agno_ls ./agnols
+ln -sf ../../agno_mk ./agnomk
+ln -sf ../../agno_rm ./agnorm
 chmod +x agno*
